d67e8973e2fca6e6bd89ae6a048c7541efdc3cc1
KODE <84865119+kimseoungyun@users.noreply.github.com>
2026-02-18 15:04:07 +0900

    Phase 1: 도메인 모델 정의(#1) / 폴더 경로 오류 수정

--- domain/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .action import ActionType, BrowserAction
-from .result import StepResult
-from .screen import PageState
-
-__all__ = ["ActionType", "BrowserAction", "StepResult", "PageState"]
---
+++ surfy/domain/models/__init__.py
@@ -0,0 +1,5 @@
+from .action import ActionType, BrowserAction
+from .result import StepResult
+from .screen import PageState
+
+__all__ = ["ActionType", "BrowserAction", "StepResult", "PageState"]
