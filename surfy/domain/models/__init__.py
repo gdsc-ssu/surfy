ab3c76a0026b4f06d6a006cf5fdc34e28e4d1205
KODE <84865119+kimseoungyun@users.noreply.github.com>
2026-02-23 17:09:40 +0900

    feat: Implement plan/task/SuccessCriteria and define LLMPort ABC

--- surfy/domain/models/__init__.py
+++ surfy/domain/models/__init__.py
@@ -1,5 +1,14 @@
 from surfy.domain.models.action import ActionType, BrowserAction
+from surfy.domain.models.plan import Plan, SuccessCriteria, Task
 from surfy.domain.models.result import StepResult
 from surfy.domain.models.screen import PageState
 
-__all__ = ["ActionType", "BrowserAction", "PageState", "StepResult"]
+__all__ = [
+    "ActionType",
+    "BrowserAction",
+    "PageState",
+    "StepResult",
+    "Plan",
+    "Task",
+    "SuccessCriteria",
+]
