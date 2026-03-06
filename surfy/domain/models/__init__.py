d9933e6d864360ba26642fdb16cb423bfb560f32
rover0811 <rover0811@hotmail.com>
2026-03-07 04:42:19 +0900

    feat: add Task/ActorOutput models and ActorService ReAct loop #6
    - Add Task model (domain/models/task.py)
    - Add ActorOutput model with to_browser_action() (domain/models/actor.py)
    - Add ActorService with ReAct while loop (domain/services/actor.py)
    - Export new models and service in __init__.py

--- surfy/domain/models/__init__.py
+++ surfy/domain/models/__init__.py
@@ -1,5 +1,7 @@
 from surfy.domain.models.action import ActionType, BrowserAction
+from surfy.domain.models.actor import ActorOutput
 from surfy.domain.models.result import StepResult
 from surfy.domain.models.screen import PageState
+from surfy.domain.models.task import Task
 
-__all__ = ["ActionType", "BrowserAction", "PageState", "StepResult"]
+__all__ = ["ActionType", "ActorOutput", "BrowserAction", "PageState", "StepResult", "Task"]
---
+++ surfy/domain/services/__init__.py
@@ -0,0 +1,3 @@
+from surfy.domain.services.actor import ActorService
+
+__all__ = ["ActorService"]
