9aff68bd817e3c96c4bf1dbc2c9263ff77d62fca
rover0811 <rover0811@hotmail.com>
2026-03-19 16:48:06 +0900

    fix(extension): add overflow-y-auto to timeline container to prevent input overlap

--- extension/src/sidepanel/App.tsx
+++ extension/src/sidepanel/App.tsx
@@ -531,7 +531,7 @@ export default function App() {
         </div>
       </header>
 
-      <div className="flex-1 min-h-0 p-3">
+      <div className="flex-1 min-h-0 overflow-y-auto p-3">
         <MessageList
           messages={state.messages}
           onInterruptAction={(value) => {
